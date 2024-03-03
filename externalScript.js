7bee9ede7009d8aecde5f99428b4fa7eef73f393
Alejandro M. Moreno <alejandromorenomuller@gmail.com>
2024-03-03 17:23:15 -0600

    Add scoring system

--- externalScript.js
+++ externalScript.js
@@ -15,59 +15,83 @@ function getComputerChoice(){
     return randomChoice
 }
 
-let playerSelection = prompt("Enter a choice between rock, paper, or scissors: ", "").toLowerCase();
-let computerSelection = getComputerChoice();
-
-
-function singleRound(playerSelection,computerSelection){
+function singleRound(playerSelection,computerSelection,computerScore,playerScore){
     if (playerSelection === "rock"){
         if (computerSelection === "rock"){
-            console.log("It's a tie with two rocks!");
-            return;
+            return "It's a tie with two rocks!";
         }
         if (computerSelection === "scissors"){
-            console.log("The user wins with their rock against the computer's scissors!");
-            return;
+            return "The user wins with their rock against the computer's scissors!";
         }
         if (computerSelection === "paper"){
-            console.log("The computer wins with their paper against the user's rock!");
-            return;
+            return "The computer wins with their paper against the user's rock!";
         }
 
     }
     else if (playerSelection === "scissors"){
         if (computerSelection === "rock"){
-            console.log("The computer wins with their rock against the user's scissors!");
-            return;
+            return "The computer wins with their rock against the user's scissors!";
         }
         if (computerSelection === "scissors"){
-            console.log("It's a tie!");
-            return;
+            return "It's a tie!";
         }
         if (computerSelection === "paper"){
-            console.log("The user wins with their scissors against the computer's paper!");
-            return;
+            return "The user wins with their scissors against the computer's paper!";
         }
     }
 
     else if (playerSelection === "paper"){
         if (computerSelection === "rock"){
-            console.log("The user wins with their paper against the compuoter's rock!");
-            return;
+            return "The user wins with their paper against the computer's rock!";
         }
         if (computerSelection === "scissors"){
-            console.log("The computer wins with their scissors against the computer's paper!");
-            return;
+            return "The computer wins with their scissors against the computer's paper!";
         }
         if (computerSelection === "paper"){
-            console.log("It's a tie!");
-            return;
+            return "It's a tie!";
         }
-    }
-    else {
-        // this means that the user did not enter a correct input
     }
 }
 
-singleRound(playerSelection,computerSelection);
+function playGame(){
+    let result;
+    let playerSelection;
+    let computerSelection;
+    let computerScore = 0;
+    let playerScore = 0;
+    for (let i = 1; i<=5; i++){
+        playerSelection = prompt("Enter a choice between rock, paper, or scissors: ", "").toLowerCase();
+        while (invalidInput(playerSelection)){
+            console.log("Your input was invalid. Try again by entering either rock, paper, or scissors");
+            playerSelection = prompt("Enter a choice between rock, paper, or scissors: ", "").toLowerCase();
+        }
+        computerSelection = getComputerChoice();
+        result = singleRound(playerSelection,computerSelection,computerScore,playerScore);
+        if (result.slice(0,5) === "The u"){
+            playerScore += 1
+        }
+        else if (result.slice(0,5) === "The c"){
+            computerScore += 1
+        }
+        console.log(result);
+    }
 
+    if (computerScore > playerScore){
+        console.log("The computer won the game!");
+    }
+    else if (playerScore > computerScore){
+        console.log("The user won the game!");
+    }
+    else{
+        console.log("The game was a tie!")
+    }
+}
+
+function invalidInput(playerSelection){
+    if (playerSelection === "scissors" || playerSelection === "rock" || playerSelection === "paper"){
+        return false
+    }
+    return true
+}
+
+playGame();
