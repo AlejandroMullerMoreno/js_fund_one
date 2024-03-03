53c1d569705808f08e37ddff042241c5d09c727f
Alejandro M. Moreno <alejandromorenomuller@gmail.com>
2024-03-03 17:32:32 -0600

    Implement a scoring system with global variables instead of through analyzing the string result of each round

--- externalScript.js
+++ externalScript.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let playerScore = 0;
+
 function getComputerChoice(){
     let randomChoice = Math.floor(Math.random() * 3)
     switch(randomChoice) { 
@@ -15,36 +18,42 @@ function getComputerChoice(){
     return randomChoice
 }
 
-function singleRound(playerSelection,computerSelection,computerScore,playerScore){
+function singleRound(playerSelection,computerSelection){
     if (playerSelection === "rock"){
         if (computerSelection === "rock"){
             return "It's a tie with two rocks!";
         }
         if (computerSelection === "scissors"){
+            playerScore += 1;
             return "The user wins with their rock against the computer's scissors!";
         }
         if (computerSelection === "paper"){
+            computerScore += 1;
             return "The computer wins with their paper against the user's rock!";
         }
 
     }
     else if (playerSelection === "scissors"){
         if (computerSelection === "rock"){
+            computerScore += 1;
             return "The computer wins with their rock against the user's scissors!";
         }
         if (computerSelection === "scissors"){
             return "It's a tie!";
         }
         if (computerSelection === "paper"){
+            playerScore += 1;
             return "The user wins with their scissors against the computer's paper!";
         }
     }
 
     else if (playerSelection === "paper"){
         if (computerSelection === "rock"){
+            playerScore += 1;
             return "The user wins with their paper against the computer's rock!";
         }
         if (computerSelection === "scissors"){
+            computerScore += 1;
             return "The computer wins with their scissors against the computer's paper!";
         }
         if (computerSelection === "paper"){
@@ -57,8 +66,6 @@ function playGame(){
     let result;
     let playerSelection;
     let computerSelection;
-    let computerScore = 0;
-    let playerScore = 0;
     for (let i = 1; i<=5; i++){
         playerSelection = prompt("Enter a choice between rock, paper, or scissors: ", "").toLowerCase();
         while (invalidInput(playerSelection)){
@@ -66,13 +73,8 @@ function playGame(){
             playerSelection = prompt("Enter a choice between rock, paper, or scissors: ", "").toLowerCase();
         }
         computerSelection = getComputerChoice();
-        result = singleRound(playerSelection,computerSelection,computerScore,playerScore);
-        if (result.slice(0,5) === "The u"){
-            playerScore += 1
-        }
-        else if (result.slice(0,5) === "The c"){
-            computerScore += 1
-        }
+        result = singleRound(playerSelection,computerSelection);
+        console.log(computerScore,playerScore)
         console.log(result);
     }
 
